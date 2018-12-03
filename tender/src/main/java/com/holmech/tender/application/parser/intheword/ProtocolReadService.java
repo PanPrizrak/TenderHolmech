a3d81f4dae01711fda9c587f5d80b9fd66055884
PanPrizrak <asirozh@gmail.com>
2018-12-03 21:56:01 +0300

    report protocol read

---
+++ tender/src/main/java/com/holmech/tender/application/parser/intheword/ProtocolReadService.java
@@ -0,0 +1,15 @@
+package com.holmech.tender.application.parser.intheword;
+
+public class ProtocolReadService {
+    /*JasperPrint jp1 = JasperFillManager.fillReport(url.openStream(), parameters,
+            new JRBeanCollectionDataSource(inspBean));
+    JasperPrint jp2 = JasperFillManager.fillReport(url.openStream(), parameters,
+            new JRBeanCollectionDataSource(inspBean));
+
+    List pages = jp2 .getPages();
+    for (int j = 0; j < pages.size(); j++) {
+        JRPrintPage object = (JRPrintPage)pages.get(j);
+        jp1.addPage(object);
+    }
+    JasperViewer.viewReport(jp1,false);*/
+}
